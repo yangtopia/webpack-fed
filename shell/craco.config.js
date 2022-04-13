d4be855c4c6a46428358563b90fd8fd2dbd39155
Heechang Yang <yangtopia@gmail.com>
2022-04-13 17:01:53 +0900

    fixed

--- shell/craco.config.js
+++ shell/craco.config.js
@@ -13,7 +13,8 @@ module.exports = {
     },
     plugins: [
       new ModuleFederationPlugin({
-        name: 'container',
+        name: 'shell',
+        filename: 'remoteEntry.js',
         remotes: {
           app1: 'app1@http://localhost:3001/remoteEntry.js',
           app2: 'app2@http://localhost:3002/remoteEntry.js',
---
+++ shell/src/atoms/shell.ts
@@ -0,0 +1,6 @@
+import { atom } from 'recoil';
+
+export const shellState = atom({
+  key: 'shellState',
+  default: 'shell_state',
+});
