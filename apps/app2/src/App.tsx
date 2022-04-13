a4dbe8a1f5e05c36fab02a72a034c2f6d075f8ce
Heechang Yang <yangtopia@gmail.com>
2022-04-13 19:23:44 +0900

    make module app stand-alone

--- apps/app2/src/App.tsx
+++ apps/app2/src/App.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import './App.scss';
 import 'antd/dist/antd.css';
 import Container from './components/Container';
+import {RecoilRoot} from "recoil";
 
 function App() {
   return (
-    <Container/>
+    <RecoilRoot override={false}>
+      <Container/>
+    </RecoilRoot>
   );
 }
 
